80370572c9e06d1f19aca8f0467169ebe37067e8
Shobhit Gangwal <gangwal28202@gmail.com>
2023-09-28 18:50:17 +0530

    Dialog Box with Start Conversation Button

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -21,7 +21,17 @@ public class DialogueManager : MonoBehaviour {
 		activeMessage = 0;
 
 		Debug.Log("Started conversation! Loaded messages: " + messages.Length);
+		DisplayMessage();
 	}
+
+	void DisplayMessage()
+	{
+		Message messageToDisplay = currentMessages[activeMessage];
+		messageText.text = messageToDisplay.message;
+
+		Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+        actorImage.sprite = actorToDisplay.sprite;
+    }
 	
 	void Start () {
 		

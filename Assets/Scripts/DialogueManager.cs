409687c665776335634697730385dfc60131f445
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-10-02 20:41:59 +0530

    Corrected Dialog Box Interaction, similar to Shobhit's implenetation of box key

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -19,7 +19,7 @@ public class DialogueManager : MonoBehaviour
 	Actor[] currentActors;
 	int activeMessage = 0;
 
-	public static bool isActive = false;
+	public bool isActive = false;
 
 
 	public void OpenDialogue(Message[] messages, Actor[] actors)
--- Assets/Scripts/NpcCharacteristics.cs
+++ Assets/Scripts/NpcCharacteristics.cs
@@ -5,35 +5,46 @@ using UnityEngine;
 
 public class NpcCharacteristics : MonoBehaviour
 {
-    
+    private Transform player;
+    [SerializeField] private float interactionDistance = 1;
+    private DialogueTrigger dialogueTrigger;
+    private DialogueManager dialogueManager;
     
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+        dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+        if (distanceToPlayer <= interactionDistance && Input.GetKeyDown(KeyCode.Space) && dialogueManager.isActive == false)
+        {
+            dialogueTrigger.StartDialog();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Triggered Collider");
-        if (other.gameObject.tag == "Player")
-        {
-            // GetComponent<DialogueTrigger>().StartDialog();
-        }
+        // Debug.Log("Triggered Collider");
+        // if (other.gameObject.tag == "Player")
+        // {
+        //     // GetComponent<DialogueTrigger>().StartDialog();
+        // }
 
     }
     
     void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("Triggered Collider Stay");
-        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<DialogueTrigger>().StartDialog();
-        }
+        // Debug.Log("Triggered Collider Stay");
+        // if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     GetComponent<DialogueTrigger>().StartDialog();
+        // }
 
     }
 }

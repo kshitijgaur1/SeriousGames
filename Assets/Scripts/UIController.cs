db2355483fc66b9dfc748b41c9d9aac35c2de8f7
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-11-11 20:23:12 +0530

    Made Remote Keyboard Accessbile

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,25 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIController: MonoBehaviour
+public class UIController : MonoBehaviour
 {
-    [SerializeField]public Canvas canvas;
+    [SerializeField] public Canvas canvas;
+
+    PlayerMovement playerMovement;
+
     // Start is called before the first frame update
     void Start()
     {
         canvas.enabled = false;
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
     }
 
     public void ShowCanvas()
     {
         canvas.enabled = true;
+        playerMovement.enabled = false;
     }
-    
+
     public void HideCanvas()
     {
         canvas.enabled = false;
+        playerMovement.enabled = true;
     }
-    
+
 
     public void BtnClicked()
     {

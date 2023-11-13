03afbb4015fa265c0a46e1d217b186b654137921
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-11-13 18:18:14 +0530

    Added PC trigger from Scene "HOME" to Scene "LAPTOP SCREEN"

---
+++ Assets/Scripts/StartPC.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartPC : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    [SerializeField] float interactionDistance = 2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactionDistance >= Vector2.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("PC Scene Started");
+                SceneManager.LoadScene("LAPTOP SCREEN");
+            }
+        }
+    }
+}

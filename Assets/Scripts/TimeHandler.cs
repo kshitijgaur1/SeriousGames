5f3f5da5e0247e8a3ddc7ece32a342f9213eec79
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-11-13 22:09:12 +0530

    Time Implemented in UI

--- Assets/Scripts/TimeHandler.cs
+++ Assets/Scripts/TimeHandler.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class TimeHandler : MonoBehaviour
 {
     public float timeStamp = 0;
+    [SerializeField] private TextMeshProUGUI timeTextField;
 
     // Start is called before the first frame update
     void Start()
     {
+        timeStamp = 0;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (!(Time.time - timeStamp > 1)) return;
-        timeStamp = Time.time;
-        Debug.Log(timeStamp);
+        timeStamp += Time.deltaTime;
+        // Debug.Log(timeStamp);
+        timeTextField.text = System.TimeSpan.FromSeconds(timeStamp).ToString("hh':'mm':'ss");
     }
 }

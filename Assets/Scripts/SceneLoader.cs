eae7e8b8c8a55ba8d1c4b5d5db8fdfb26c9952b6
Shobhit Gangwal <gangwal28202@gmail.com>
2023-11-18 12:41:05 +0530

    Scene Loader added to timeline

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("HOME", LoadSceneMode.Single);
     }
 }

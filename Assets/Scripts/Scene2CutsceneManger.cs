e35d1617d6bf9cb8d280d90f670ede97fbac168e
Shobhit Gangwal <gangwal28202@gmail.com>
2023-11-24 16:23:32 +0530

    Scene2 cutscene canvas

--- Assets/Scripts/Scene2CutsceneManger.cs
+++ Assets/Scripts/Scene2CutsceneManger.cs
@@ -1,23 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Scene2CutsceneManger : MonoBehaviour
 {
+    public Canvas canvas;
     public NpcCharacteristics npc1;
     public NpcCharacteristics npc2;
     bool cutsceneStarted = false;
-    void Start()
-    {
-        
-    }
+    public PlayableDirector cutsceneDirector;
 
     // Update is called once per frame
+    private void Start()
+    {
+        canvas.enabled = false;
+    }
+
     void Update()
     {
         if (!cutsceneStarted && npc1.guidelineRead && npc2.guidelineRead && !npc1.dialogueManager.isActive && !npc2.dialogueManager.isActive) {
             cutsceneStarted = true;
-            Debug.Log("Cutscene");
+            StartCoroutine(StartCutscene());
         }
+    }
+
+    IEnumerator StartCutscene()
+    {
+        Debug.Log("Cutscene");
+        canvas.enabled = true;
+        if (cutsceneDirector != null)
+        {
+            cutsceneDirector.Play();
+        }
+        yield return new WaitForSeconds((float)cutsceneDirector.duration);
+        canvas.enabled = false;
     }
 }

8c9939856d4df88f72b0a031e7a0f52c145d4538
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-11-30 06:41:20 +0530

    Cell Modification

---
+++ Assets/Scripts/ValidateDataCell.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidateDataCell : MonoBehaviour
+{
+    [SerializeField] String CellKey;
+    [SerializeField] GameObject Cell;
+    [SerializeField] Image CellBackground;
+    [SerializeField] TMP_InputField CellText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("CellText.text: " + CellText.text + " CellKey: " + CellKey);
+        int CellTextInt = int.Parse(CellText.text);
+        
+        if (CellKey == "Bill Amt" && CellTextInt == 3500)
+        {
+            // Debug.Log("Bill Amt is 3500");
+            // Debug.Log(CellTextInt);
+            CellBackground.color = new Color(0.38f, 1f, 0.7f);
+        }
+        
+        if (CellKey == "Total Bill Amt" && CellTextInt == 16100)
+        {
+            // Debug.Log("Total Bill Amt is 16100");
+            // Debug.Log(CellTextInt);
+            CellBackground.color = new Color(0.38f, 1f, 0.7f);
+        }
+        
+        if (CellKey == "Total Balance" && CellTextInt == 2400)
+        {
+            // Debug.Log("Total Balance is 3500");
+            // Debug.Log(CellTextInt);
+            CellBackground.color = new Color(0.38f, 1f, 0.7f);
+        }
+        
+    }
+}
--- Assets/Scripts/ValidateLevel.cs
+++ Assets/Scripts/ValidateLevel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExcelCellFormatting : MonoBehaviour
+public class ValidateLevel : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

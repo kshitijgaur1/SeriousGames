0435286d728ddfdef5c10caf7d5815c301078521
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2023-12-05 04:45:12 +0530

    Implemented cutscene hovering in introduction

--- Assets/Scripts/SkipCutScene.cs
+++ Assets/Scripts/SkipCutScene.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class SkipCutScene : MonoBehaviour
 {
+    private PlayableDirector director;
+    int timeStep=7;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        director = GetComponent<PlayableDirector>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) ||
-            Input.GetKeyDown(KeyCode.KeypadEnter))
+            Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            SceneManager.LoadScene("HOME", LoadSceneMode.Single);
-        }
+            if (director.time - (director.time % timeStep) + timeStep >= (float)director.duration)
+                SceneManager.LoadScene("HOME", LoadSceneMode.Single);
+            else
+            {
+                director.time = director.time - (director.time % timeStep) + timeStep;
+                director.Evaluate();
+            }
             
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(director.time - (director.time % timeStep) - timeStep <= 7f)
+                director.time = 0f;
+            else
+                director.time = director.time - (director.time % timeStep) - timeStep; 
+            director.Evaluate();
+        }
     }
 }
